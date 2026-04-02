a19ace48b6fd8fd42ad7f3a9e210b72caf9240c6
Jonatan Lindell <mail@jonatanlindell.com>
2026-04-03 00:15:48 +0200

    init

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+int main(void)
+{
+	const struct device *dev;
+	int ret;
+
+	// dev = device_get_binding("GPIO_0");
+	// if (dev == NULL) {
+	// 	return;
+	// }
+	// if (ret < 0) {
+	// 	return;
+	// }
+
+	while (1) {
+	}
+}
